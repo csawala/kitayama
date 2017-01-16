a125d8161fae22ca010cbab87d17df481da06479
csawala <chris.sawala@gmail.com>
2017-01-16 23:15:43 +0000

    added temp buttons w/ basic styling

--- src/Nav.jsx
+++ src/Nav.jsx
@@ -4,9 +4,29 @@ class Nav extends Component {
   render() {
     return (
       <div className="navbar">
-        <img src="../build/CMACcrest.png" className="cmac-crest" />
-        <h1>CMAC Kita Yama Dojo</h1>
-        <img src="../build/CMACfist.png" className="cmac-fist"/>
+        <div className="title-bar">
+          <img src="../build/CMACcrest.png" className="cmac-crest" />
+          <h1>CMAC Kita Yama Dojo</h1>
+          <img src="../build/CMACfist.png" className="cmac-fist"/>
+        </div>
+
+        <div className="button-bar">
+          <a href='#' className="navbutton" onClick={console.log('cmac')}>
+            CMAC
+          </a>
+          <a href='#' className="navbutton" onClick={console.log('instructor')}>
+            Instructor
+          </a>
+          <a href='#' className="navbutton" onClick={console.log('schedule')}>
+            Schedule
+          </a>
+          <a href='#' className="navbutton" onClick={console.log('events')}>
+            Events
+          </a>
+          <a href='#' className="navbutton" onClick={console.log('contact')}>
+            Contact us
+          </a>
+        </div>
       </div>
     )
   }
